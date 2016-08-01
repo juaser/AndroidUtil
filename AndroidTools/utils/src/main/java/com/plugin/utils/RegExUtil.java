05b2e7fdeda503dcd1e702781121bb421358084a
ZhangXingLei <zxl@reachjunction.com>
2016-08-01 11:07:39 +0800

    kjj

---
+++ AndroidTools/utils/src/main/java/com/plugin/utils/RegExUtil.java
@@ -0,0 +1,14 @@
+package com.plugin.utils;
+
+/**
+ * @类说明：正则
+ * @author：zxl
+ * @CreateTime 2016/8/1.
+ */
+public class RegExUtil {
+    /**
+     * 中英文数字
+     */
+    public static final String Word_REG = "[^0-9a-zA-Z\u4E00-\u9FA5_]";
+    public static final String NUM_REG = "[^0-9]";
+}
