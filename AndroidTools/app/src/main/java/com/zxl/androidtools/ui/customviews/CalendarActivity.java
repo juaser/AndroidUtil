5242ce1c49c1f426e6e744906c39471350e8cab8
ZhangXingLei <zxl@reachjunction.com>
2016-09-06 10:58:59 +0800

    日历

---
+++ AndroidTools/app/src/main/java/com/zxl/androidtools/ui/customviews/CalendarActivity.java
@@ -0,0 +1,43 @@
+package com.zxl.androidtools.ui.customviews;
+
+import android.widget.TextView;
+
+import com.plugin.utils.base.BaseActivity;
+import com.plugin.weight.calendar.KCalendarPop;
+import com.zxl.androidtools.R;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+
+/**
+ * @Description:
+ * @Author: zxl
+ * @Date: 6/9/16 上午10:40.
+ */
+public class CalendarActivity extends BaseActivity {
+    @Bind(R.id.tv_calendar)
+    TextView tvCalendar;
+    @Bind(R.id.tv_calendar_date)
+    TextView tvCalendarDate;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_calendar;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @OnClick(R.id.tv_calendar)
+    void show() {
+        new KCalendarPop(this, tvCalendar, new KCalendarPop.onCompleteListener() {
+            @Override
+            public void complete(String date) {
+                tvCalendarDate.setText(date);
+            }
+        });
+    }
+
+}
