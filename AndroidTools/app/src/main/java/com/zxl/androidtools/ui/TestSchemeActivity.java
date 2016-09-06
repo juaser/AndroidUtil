fc5f2899b0f3f28a5f30a3fae4a73e0e63109d94
ZhangXingLei <zxl@reachjunction.com>
2016-09-06 12:06:32 +0800

    scheme启动activity

---
+++ AndroidTools/app/src/main/java/com/zxl/androidtools/ui/TestSchemeActivity.java
@@ -0,0 +1,72 @@
+package com.zxl.androidtools.ui;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.plugin.utils.base.BaseActivity;
+import com.zxl.androidtools.R;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+
+/**
+ * @Description:
+ * @Author: zxl
+ * @Date: 6/9/16 上午11:17.
+ */
+public class TestSchemeActivity extends BaseActivity {
+
+    @Bind(R.id.tv_jump)
+    TextView tvJump;
+    @Bind(R.id.tv_jump_text)
+    TextView tvJumpText;
+    @Bind(R.id.et_scheme_authoeity)
+    EditText etSchemeAuthoeity;
+    @Bind(R.id.et_scheme_path)
+    EditText etSchemePath;
+    @Bind(R.id.et_scheme_query)
+    EditText etSchemeQuery;
+    @Bind(R.id.et_scheme_fragment)
+    EditText etSchemeFragment;
+
+    private String str_authority;
+    private String str_path;
+    private String str_query;
+    private String str_fragment;
+
+    private String str_jumpscheme = "";
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_testscheme;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @OnClick(R.id.tv_jump)
+    void jump() {
+        getData();
+        tvJumpText.setText("scheme:\n" + str_jumpscheme);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(str_jumpscheme));
+        startActivity(intent);
+    }
+
+    /**
+     * [scheme:][//authority][path][?query][#fragment]
+     * 例如：http://www.java2s.com:8080/yourpath/fileName.htm?stove=10&path=32&id=4#harvic
+     */
+    public void getData() {
+        str_authority = etSchemeAuthoeity.getText().toString().trim();
+        str_path = etSchemePath.getText().toString().trim();
+        str_query = etSchemeQuery.getText().toString().trim();
+        str_fragment = etSchemeFragment.getText().toString().trim();
+        str_jumpscheme = getString(R.string.str_scheme_testscheme) + "://" + str_authority + "/" + str_path + "?" + str_query + "#" + str_fragment;
+    }
+}
---
+++ AndroidTools/app/src/main/java/com/zxl/androidtools/ui/TestSchemeResultActivity.java
@@ -0,0 +1,53 @@
+package com.zxl.androidtools.ui;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import com.plugin.utils.base.BaseActivity;
+import com.zxl.androidtools.R;
+
+import butterknife.Bind;
+
+/**
+ * @Description: 通过scheme 启动此Activity  [scheme:][//authority][path][?query][#fragment]
+ * @Author: zxl
+ * @Date: 6/9/16 上午11:02.
+ */
+public class TestSchemeResultActivity extends BaseActivity {
+    @Bind(R.id.tv_text)
+    TextView tvText;
+
+    private String str_authority;
+    private String str_path;
+    private String str_query;
+    private String str_fragment;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_testscheme_result;
+    }
+
+    @Override
+    public void initView() {
+        doIntent(getIntent());
+    }
+
+    public void doIntent(Intent intent) {
+        if (null != intent && null != intent.getAction() && null != intent.getScheme()
+                && TextUtils.equals(intent.getScheme(), getString(R.string.str_scheme_testscheme))) {
+            Uri schemeUri = intent.getData();
+            str_authority = schemeUri.getAuthority();
+            str_path = schemeUri.getPath();
+            str_query = schemeUri.getQuery();
+            str_fragment = schemeUri.getFragment();
+            tvText.setText("str_authority==" + str_authority +
+                    "\nstr_path==" + str_path +
+                    "\nstr_query==" + str_query +
+                    "\nstr_fragment==" + str_fragment);
+        } else {
+            tvText.setText("不是通过Scheme跳转");
+        }
+    }
+}
