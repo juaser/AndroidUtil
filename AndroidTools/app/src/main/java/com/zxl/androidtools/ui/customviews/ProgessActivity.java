d03dbc85b01a02fc73024fa007b4fdeb55f75f15
ZhangXingLei <zxl@reachjunction.com>
2016-09-12 12:04:33 +0800

    带百分比的进度条

--- AndroidTools/app/src/main/java/com/zxl/androidtools/ui/customviews/ProgessActivity.java
+++ AndroidTools/app/src/main/java/com/zxl/androidtools/ui/customviews/ProgessActivity.java
@@ -5,7 +5,11 @@ import android.widget.TextView;
 
 import com.plugin.utils.base.BaseAppCompatActivity;
 import com.plugin.weight.progressbar.HorizontalProgressView;
+import com.plugin.weight.progressbar.NumberProgressBar;
 import com.zxl.androidtools.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.Bind;
 import butterknife.OnClick;
@@ -22,8 +26,11 @@ public class ProgessActivity extends BaseAppCompatActivity {
     TextView tvTimer;
     @Bind(R.id.progress_horizontal)
     ProgressBar progressHorizontal;
+    @Bind(R.id.numberPrgogessBar)
+    NumberProgressBar numberPrgogessBar;
 
     private boolean isTimer = false;
+    private Timer timer;
 
     @Override
     public int getLayoutId() {
@@ -33,6 +40,18 @@ public class ProgessActivity extends BaseAppCompatActivity {
     @Override
     public void initView() {
         progressHorizontal.setProgress(50);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        numberPrgogessBar.incrementProgressBy(1);
+                    }
+                });
+            }
+        }, 0, 1000);
     }
 
 
@@ -41,5 +60,11 @@ public class ProgessActivity extends BaseAppCompatActivity {
         isTimer = !isTimer;
         numperBar.setStartTimer(isTimer);
         tvTimer.setText(isTimer ? getString(R.string.str_stop_timer) : getString(R.string.str_start_timer));
+    }
+
+    @Override
+    protected void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
     }
 }
