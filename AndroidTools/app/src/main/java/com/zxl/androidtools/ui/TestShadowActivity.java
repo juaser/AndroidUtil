fd52dd924617baa445ad8ce54fcb295924f87acf
ZhangXingLei <zxl@reachjunction.com>
2016-09-12 13:24:11 +0800

    layer-list制作阴影

---
+++ AndroidTools/app/src/main/java/com/zxl/androidtools/ui/TestShadowActivity.java
@@ -0,0 +1,34 @@
+package com.zxl.androidtools.ui;
+
+import android.widget.TextView;
+
+import com.plugin.utils.base.BaseAppCompatActivity;
+import com.zxl.androidtools.R;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+
+/**
+ * @Description:
+ * @Author: zxl
+ * @Date: 12/9/16 PM12:23.
+ */
+public class TestShadowActivity extends BaseAppCompatActivity {
+    @Bind(R.id.tv_shadow)
+    TextView tvShadow;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_shadow;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @OnClick(R.id.tv_shadow)
+    void shadow() {
+
+    }
+}
