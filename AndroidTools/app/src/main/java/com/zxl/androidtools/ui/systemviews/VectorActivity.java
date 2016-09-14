4b6fd76b3b852ee04d546503d9c546930ecc4e7d
ZhangXingLei <zxl@reachjunction.com>
2016-09-14 17:48:28 +0800

    矢量图的使用

---
+++ AndroidTools/app/src/main/java/com/zxl/androidtools/ui/systemviews/VectorActivity.java
@@ -0,0 +1,21 @@
+package com.zxl.androidtools.ui.systemviews;
+
+import com.plugin.utils.base.BaseAppCompatActivity;
+import com.zxl.androidtools.R;
+
+/**
+ * @Description:
+ * @Author: zxl
+ * @Date: 14/9/16 PM5:37.
+ */
+public class VectorActivity extends BaseAppCompatActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_vector;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
