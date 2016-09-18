e1440cf51ab2bb3028d51a3fa09f94d958d9489d
ZhangXingLei <zxl@reachjunction.com>
2016-09-18 15:15:38 +0800

    涟漪效果

---
+++ AndroidTools/app/src/main/java/com/zxl/androidtools/ui/customviews/RippleActivity.java
@@ -0,0 +1,20 @@
+package com.zxl.androidtools.ui.customviews;
+
+import com.plugin.utils.base.BaseAppCompatActivity;
+import com.zxl.androidtools.R;
+
+/**
+ * @Description:
+ * @Author: zxl
+ * @Date: 18/9/16 PM2:41.
+ */
+public class RippleActivity extends BaseAppCompatActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_ripple;
+    }
+
+    @Override
+    public void initView() {
+    }
+}
