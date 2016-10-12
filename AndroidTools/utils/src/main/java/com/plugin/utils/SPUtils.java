c056d9b9daa10896cf82d91dd78774107c04fe17
ZhangXingLei <zxl@reachjunction.com>
2016-10-12 17:35:19 +0800

    SPUtils 修改

--- AndroidTools/utils/src/main/java/com/plugin/utils/SPUtils.java
+++ AndroidTools/utils/src/main/java/com/plugin/utils/SPUtils.java
@@ -3,8 +3,6 @@ package com.plugin.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.plugin.utils.manager.AppManager;
-
 
 /**
  * @Description: SP读写工具类
@@ -15,202 +13,136 @@ public class SPUtils {
 
     private static volatile SPUtils mInstance = null;
 
-    private SPUtils() {
+    /**
+     * SP的name值
+     */
+    public static String PREFERENCE_NAME = "ANCROID_UTIL_CODE";
+
+    private static SharedPreferences sharedPreferences = null;
+
+    private SPUtils(Context context) {
+        sharedPreferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
     }
 
-    public static SPUtils getInstance() {
+    public static SPUtils getInstance(Context context) {
         SPUtils instance = mInstance;
         if (instance == null) {
             synchronized (SPUtils.class) {
                 instance = mInstance;
                 if (instance == null) {
-                    instance = new SPUtils();
+                    instance = new SPUtils(context);
                     mInstance = instance;
                 }
             }
+        }
+        if (sharedPreferences == null) {
+            sharedPreferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
         }
         return instance;
     }
 
     /**
-     * @description: 获取上下文
-     */
-    public Context getContext() {
-        return AppManager.getInstance().getTop();
-    }
-
-    /**
-     * SP的name值
-     * <p>可通过修改PREFERENCE_NAME变量修改SP的name值</p>
-     */
-    public static String PREFERENCE_NAME = "ANCROID_UTIL_CODE";
-
-    /**
      * SP中写入String类型value
-     *
-     * @param key   键
-     * @param value 值
-     * @return true: 写入成功<br>false: 写入失败
      */
-    public  boolean putString(String key, String value) {
-        return getSP().edit().putString(key, value).commit();
+    public boolean putString(String key, String value) {
+        return sharedPreferences.edit().putString(key, value).commit();
     }
 
     /**
-     * SP中读取String
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值null
+     * SP中读取String 不存在返回默认值null
      */
-    public  String getString(String key) {
-        return getString( key, null);
+    public String getString(String key) {
+        return getString(key, null);
     }
 
     /**
-     * SP中读取String
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值defaultValue
+     * SP中读取String  不存在返回默认值defaultValue
      */
-    public  String getString(String key, String defaultValue) {
-        return getSP().getString(key, defaultValue);
+    public String getString(String key, String defaultValue) {
+        return sharedPreferences.getString(key, defaultValue);
     }
 
     /**
      * SP中写入int类型value
-     *
-     * @param key   键
-     * @param value 值
-     * @return true: 写入成功<br>false: 写入失败
      */
-    public  boolean putInt(String key, int value) {
-        return getSP().edit().putInt(key, value).commit();
+    public boolean putInt(String key, int value) {
+        return sharedPreferences.edit().putInt(key, value).commit();
     }
 
     /**
-     * SP中读取int
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值-1
+     * SP中读取int 不存在返回默认值-1
      */
-    public  int getInt(String key) {
+    public int getInt(String key) {
         return getInt(key, -1);
     }
 
     /**
-     * SP中读取int
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值defaultValue
+     * SP中读取int 不存在返回默认值defaultValue
      */
-    public  int getInt(String key, int defaultValue) {
-        return getSP().getInt(key, defaultValue);
+    public int getInt(String key, int defaultValue) {
+        return sharedPreferences.getInt(key, defaultValue);
     }
 
     /**
      * SP中写入long类型value
-     *
-     * @param key   键
-     * @param value 值
-     * @return true: 写入成功<br>false: 写入失败
      */
-    public  boolean putLong(String key, long value) {
-        return getSP().edit().putLong(key, value).commit();
+    public boolean putLong(String key, long value) {
+        return sharedPreferences.edit().putLong(key, value).commit();
     }
 
     /**
-     * SP中读取long
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值-1
+     * SP中读取long 不存在返回默认值-1
      */
-    public  long getLong(String key) {
+    public long getLong(String key) {
         return getLong(key, -1);
     }
 
     /**
-     * SP中读取long
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值defaultValue
+     * SP中读取long 不存在返回默认值defaultValue
      */
-    public  long getLong(String key, long defaultValue) {
-        return getSP().getLong(key, defaultValue);
+    public long getLong(String key, long defaultValue) {
+        return sharedPreferences.getLong(key, defaultValue);
     }
 
     /**
      * SP中写入float类型value
-     *
-     * @param key   键
-     * @param value 值
-     * @return true: 写入成功<br>false: 写入失败
      */
-    public  boolean putFloat(String key, float value) {
-        return getSP().edit().putFloat(key, value).commit();
+    public boolean putFloat(String key, float value) {
+        return sharedPreferences.edit().putFloat(key, value).commit();
     }
 
     /**
-     * SP中读取float
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值-1
+     * SP中读取float 不存在返回默认值-1
      */
-    public  float getFloat(String key) {
-        return getFloat( key, -1);
+    public float getFloat(String key) {
+        return getFloat(key, -1);
     }
 
     /**
-     * SP中读取float
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值defaultValue
+     * SP中读取float  不存在返回默认值defaultValue
      */
-    public  float getFloat(String key, float defaultValue) {
-        return getSP().getFloat(key, defaultValue);
+    public float getFloat(String key, float defaultValue) {
+        return sharedPreferences.getFloat(key, defaultValue);
     }
 
     /**
      * SP中写入boolean类型value
-     *
-     * @param key   键
-     * @param value 值
-     * @return true: 写入成功<br>false: 写入失败
      */
-    public  boolean putBoolean(String key, boolean value) {
-        return getSP().edit().putBoolean(key, value).commit();
+    public boolean putBoolean(String key, boolean value) {
+        return sharedPreferences.edit().putBoolean(key, value).commit();
     }
 
     /**
-     * SP中读取boolean
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值false
+     * SP中读取boolean 不存在返回默认值false
      */
-    public  boolean getBoolean(String key) {
+    public boolean getBoolean(String key) {
         return getBoolean(key, false);
     }
 
     /**
-     * SP中读取boolean
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值defaultValue
+     * SP中读取boolean  不存在返回默认值defaultValue
      */
-    public  boolean getBoolean(String key, boolean defaultValue) {
-        return getSP().getBoolean(key, defaultValue);
-    }
-
-    /**
-     * 获取name为PREFERENCE_NAME的SP对象
-     *
-     * @return SP
-     */
-    private SharedPreferences getSP() {
-        return getContext().getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return sharedPreferences.getBoolean(key, defaultValue);
     }
 }
