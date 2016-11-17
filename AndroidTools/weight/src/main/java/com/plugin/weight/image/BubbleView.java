809cea8552cdf030ebfcac67cb829d34c0e44cf4
zxl <807130989@qq.com>
2016-11-17 16:53:45 +0800

    爱心气泡点赞

--- AndroidTools/weight/src/main/java/com/plugin/weight/image/BubbleView.java
+++ AndroidTools/weight/src/main/java/com/plugin/weight/image/BubbleView.java
@@ -10,8 +10,6 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.RelativeLayout;
 
-import com.plugin.utils.log.LogUtils;
-
 /**
  * @Description: 制作一个可以不断冒出图片的布局
  * @Author: zxl
@@ -22,7 +20,7 @@ public class BubbleView extends RelativeLayout {
     private Context mContext;
     private int width;//控件的宽
     private int height;//控件的高
-    private int duration = 1000;//冒泡动画时间
+    private int duration = 3000;//冒泡动画时间
 
     public BubbleView(Context context) {
         this(context, null, 0);
@@ -43,7 +41,7 @@ public class BubbleView extends RelativeLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         width = getMeasuredWidth();
         height = getMeasuredHeight();
-        LogUtils.e("控件宽度：" + width + "    高度：" + height);
+//        LogUtils.e("控件宽度：" + width + "    高度：" + height);
     }
 
     public void init() {
@@ -53,6 +51,7 @@ public class BubbleView extends RelativeLayout {
     public void addView() {
         //添加一个view 在布局的最底部
         final HeartImageView heartImageView = new HeartImageView(mContext);
+        heartImageView.setVisibility(GONE);
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
         params.addRule(RelativeLayout.CENTER_HORIZONTAL);
@@ -61,7 +60,7 @@ public class BubbleView extends RelativeLayout {
         heartImageView.post(new Runnable() {
             @Override
             public void run() {
-                ValueAnimator valueAnimator = ValueAnimator.ofObject(new BubbleEvaluator(), new PointF(0, 0), new PointF(100, 100));
+                ValueAnimator valueAnimator = ValueAnimator.ofObject(new BubbleEvaluator(), new PointF(width / 2, height), new PointF(width / 2, 0));
                 valueAnimator.setDuration(duration);
                 valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                     @Override
@@ -78,6 +77,12 @@ public class BubbleView extends RelativeLayout {
                     public void onAnimationEnd(Animator animation) {
                         super.onAnimationEnd(animation);
                         removeView(heartImageView);
+                    }
+
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        super.onAnimationStart(animation);
+                        heartImageView.setVisibility(VISIBLE);
                     }
                 });
                 valueAnimator.start();
@@ -103,8 +108,8 @@ public class BubbleView extends RelativeLayout {
          */
         @Override
         public PointF evaluate(float v, PointF start, PointF end) {
-            LogUtils.e("v==" + v + "   start.x==" + start.x + "   start.y==" + start.y + "   end.x==" + end.x + "  end.y==" + end.y);
-            return new PointF(0, 0);
+//            LogUtils.e("v==" + v + "   start.x==" + start.x + "   start.y==" + start.y + "   end.x==" + end.x + "  end.y==" + end.y);
+            return new PointF(width / 2, (start.y - end.y) * (1 - v));
         }
     }
 
