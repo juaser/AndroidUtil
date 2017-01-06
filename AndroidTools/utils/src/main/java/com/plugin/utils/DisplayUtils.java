51ec9f9b1f9b56bec2fc6c033d5707b152b64432
zxl <807130989@qq.com>
2017-01-06 18:22:40 +0800

    去除无用的方法

--- AndroidTools/utils/src/main/java/com/plugin/utils/DisplayUtils.java
+++ AndroidTools/utils/src/main/java/com/plugin/utils/DisplayUtils.java
@@ -9,7 +9,6 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Build;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -109,73 +108,6 @@ public class DisplayUtils {
         return 0;
     }
 
-    /**
-     * 在onCreate()即可强行获取View的尺寸
-     * <p>需回调onGetSizeListener接口，在onGetSize中获取view宽高</p>
-     * <p>用法示例如下所示</p>
-     * <pre>
-     * SizeUtils.forceGetViewSize(view);
-     * SizeUtils.setListener(new SizeUtils.onGetSizeListener() {
-     *     <br>@Override
-     *     public void onGetSize(View view) {
-     *         Log.d("tag", view.getWidth() + " " + view.getHeight());
-     *     }
-     * });
-     * </pre>
-     *
-     * @param view 视图
-     */
-    public static void forceGetViewSize(final View view) {
-        view.post(new Runnable() {
-            @Override
-            public void run() {
-                if (mListener != null) {
-                    mListener.onGetSize(view);
-                }
-            }
-        });
-    }
-
-    /**
-     * 获取到View尺寸的监听
-     */
-    public interface onGetSizeListener {
-        void onGetSize(View view);
-    }
-
-    public static void setListener(onGetSizeListener listener) {
-        mListener = listener;
-    }
-
-    private static onGetSizeListener mListener;
-
-    /**
-     * ListView中提前测量View尺寸，如headerView
-     * <p>用的时候去掉注释拷贝到ListView中即可</p>
-     * <p>参照以下注释代码</p>
-     *
-     * @param view 视图
-     */
-    public static void measureViewInLV(View view) {
-        Log.i("tips", "U should copy the following code.");
-        /*
-        ViewGroup.LayoutParams p = view.getLayoutParams();
-        if (p == null) {
-            p = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT);
-        }
-        int width = ViewGroup.getChildMeasureSpec(0, 0, p.width);
-        int height;
-        int tempHeight = p.height;
-        if (tempHeight > 0) {
-            height = MeasureSpec.makeMeasureSpec(tempHeight,
-                    MeasureSpec.EXACTLY);
-        } else {
-            height = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
-        }
-        view.measure(width, height);
-        */
-    }
     /**
      * 获取屏幕的宽度px
      */
